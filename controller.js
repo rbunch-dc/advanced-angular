766034a8f46dd7902e8e86177a222c90d3e014ca
Robert Bunch <robert.bunch@tinroofsoftware.com>
2015-11-17 11:45:48 -0500

    Add routes

--- controller.js
+++ controller.js
@@ -1,4 +1,18 @@
-var countryApp = angular.module('countryApp',[]);
+var countryApp = angular.module('countryApp',['ngRoute']);
+
+countryApp.config(function($routeProvider){
+	$routeProvider.when('/',{
+		template: '<ul><li ng-repeat="country in countries">{{country.name}}</li></ul>',
+		controller: 'countryCntrl'		
+	}).
+	when('/:countryName',{
+		template: '<h1>Country detail will go here.</h1>',
+		controller: 'countryDetailCntrl'
+	}).
+	otherwise({
+		redirectTo: '/'
+	});
+});
 
 countryApp.controller('countryCntrl', function($scope, $http){
 
@@ -6,4 +20,8 @@ countryApp.controller('countryCntrl', function($scope, $http){
 		$scope.countries = countryData;
 	});
 
+});
+
+countryApp.controller('countryDetailCntrl', function($scope, $routeParams){
+	console.log($routeParams);
 });
